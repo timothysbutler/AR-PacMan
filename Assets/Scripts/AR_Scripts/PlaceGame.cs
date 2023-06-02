deb91d9ce73cb4562a2a55f4b864929f691e23c5
Timothy Butler <91582032+butletim@users.noreply.github.com>
2023-06-02 14:50:42 -0600

    Testing AR

---
+++ Assets/Scripts/AR_Scripts/PlaceGame.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlaceGame : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject board;
+
+    private ARRaycastManager arRaycastManager;
+    private ARPlaneManager arPlaneManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        arPlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.TouchSimulation.Disable();
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        if (finger.index != 0)
+            return;
+
+        if (arRaycastManager.Raycast(finger.currentTouch.screenPosition, hits,
+            TrackableType.PlaneWithinPolygon))
+        {
+            foreach(ARRaycastHit hit in hits)
+            {
+                Pose pose = hit.pose;
+                GameObject ogj = Instantiate(board, pose.position, pose.rotation);
+            }
+        }
+    }
+
+}
