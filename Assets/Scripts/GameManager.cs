6aaacb59408e4cad97bf6dad78121102c6c03825
Timothy Butler <91582032+butletim@users.noreply.github.com>
2023-06-05 21:49:22 -0600

    Ghost update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@
 // (4) https://noobtuts.com/unity/2d-pacman-game
 //-----------------------------------------------------------//
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -19,6 +20,7 @@ public class GameManager : MonoBehaviour
     //public PlayerController pacman;
     public Transform pellets;
     public Energizer energizer;
+    public List<GameObject> lifeList;
 
     public int ghostMulti { get; private set; }
     public int score { get; private set; }
@@ -27,23 +29,31 @@ public class GameManager : MonoBehaviour
     public AudioSource siren;
     public AudioSource munch1;
     public AudioSource munch2;
-    public AudioSource powerUp;
     public AudioSource ghostEaten;
+    public AudioSource powerUp;
+    public AudioSource startGame;
     private int munchNumber = 0;
 
     // Start the Game
     private void Start()
     {
-        NewGame();
-        siren.Play();
+        startGame.Play();
+        this.pacman.gameObject.SetActive(false);
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+        Invoke(nameof(NewGame), 5.5f);
+        
     }
 
     // Run at the beginning and at restart
     private void NewGame()
     {
+        siren.Play();
         SetScore(0);
         SetLives(3);
-        //NewRound();
+        NewRound();
     }
 
     // New Round, Set everything back and increase variables (if needed)
@@ -76,6 +86,7 @@ public class GameManager : MonoBehaviour
     // All lives have been loss, game ends
     private void GameOver()
     {
+        siren.Stop();
         for (int i = 0; i < this.ghosts.Length; i++) {
             this.ghosts[i].gameObject.SetActive(false);
         }
@@ -110,6 +121,7 @@ public class GameManager : MonoBehaviour
         this.pacman.gameObject.SetActive(false);
 
         SetLives(this.lives - 1);
+        this.lifeList[lives].gameObject.SetActive(false);
 
         if (this.lives > 0) {
             Invoke(nameof(ResetState), 3.0f);
@@ -137,23 +149,26 @@ public class GameManager : MonoBehaviour
 
         if (!CheckPelletCount())
         {
-            Debug.Log("All Gone");
-            //this.pacman.gameObject.SetActive(false);
-            //Invoke(nameof(NewRound), 3.0f);
+            this.pacman.gameObject.SetActive(false);
+            Invoke(nameof(NewRound), 3.0f);
         }
     }
 
     // If energizer gets eaten, increase score, turn ghosts to blue
     public void EnergizerEaten(Energizer energizer)
     {
-
+        powerUp.Play();
         energizer.gameObject.SetActive(false);
 
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].frightened.Enable(energizer.duration);
+        }
+
         SetScore(this.score + energizer.points);
-        //CancelInvoke();
-        //Invoke(nameof(ResetGhostMulti), energizer.duration);
-        // power up pacman
-        // ghosts scared
+        CancelInvoke();
+        Invoke(nameof(ResetGhostMulti), energizer.duration);
+        Invoke(nameof(StopSound), energizer.duration);
     }
 
     // Check the pellets if they are active
@@ -172,6 +187,11 @@ public class GameManager : MonoBehaviour
     private void ResetGhostMulti() 
     {
         this.ghostMulti = 1;
+    }
+
+    private void StopSound()
+    {
+        powerUp.Stop();
     }
 }
 
--- Assets/Scripts/Ghosts/GhostFrightened.cs
+++ Assets/Scripts/Ghosts/GhostFrightened.cs
@@ -2,4 +2,82 @@ using UnityEngine;
 
 public class GhostFrightened : GhostBehavior
 {
+    public Material newMaterial;
+    public Material oldMaterial { get; private set; }
+
+
+    public bool eaten { get; private set; }
+
+
+
+    private void Eaten()
+    {
+        this.eaten = true;
+
+        Vector3 position = this.ghost.home.inside.position;
+        this.ghost.transform.position = position;
+
+        this.ghost.GetComponent<MeshRenderer>().enabled = false;
+
+        this.ghost.home.Enable(8.0f);
+    }
+
+    public override void Enable(float duration)
+    {
+        base.Enable(duration);
+
+        oldMaterial = this.ghost.GetComponent<Renderer>().material;
+        this.ghost.GetComponent<Renderer>().material = newMaterial;
+    }
+
+    public override void Disable()
+    {
+        base.Disable();
+
+        
+    }
+
+    private void OnEnable()
+    {
+        this.ghost.movement.speedMulti = 0.5f;
+        this.eaten = false;
+    }
+
+    private void OnDisable()
+    {
+        this.ghost.movement.speedMulti = 1.0f;
+        this.eaten = false;
+        this.ghost.GetComponent<Renderer>().material = oldMaterial;
+        this.ghost.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pacman") && this.enabled)
+        {
+            Eaten();
+        }
+
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && this.enabled)
+        {
+            Vector3 direction = Vector3.zero;
+            float maxDistance = float.MinValue;
+
+            foreach (Vector3 availableDirction in node.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirction.x, availableDirction.y, availableDirction.z);
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude;
+
+                if (distance > maxDistance)
+                {
+                    direction = availableDirction;
+                    maxDistance = distance;
+                }
+            }
+
+            this.ghost.movement.SetDirection(direction);
+        }
+    }
 }
