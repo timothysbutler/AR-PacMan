7a3e5ddad91f7c17df41e73192524d1675160a87
Timothy Butler <91582032+butletim@users.noreply.github.com>
2023-06-07 23:05:47 -0600

    update ghosts scripts

--- Assets/Scripts/Ghosts/GhostBehavior.cs
+++ Assets/Scripts/Ghosts/GhostBehavior.cs
@@ -1,3 +1,14 @@
+//-----------------------------------------------------------//
+// Authors: Timothy Butler and Nick Thomas
+// Date Last Modified: June 7th, 2023
+// Course: CS 497 - 400
+// Oregon State University
+// Source(s):
+// (1) https://www.youtube.com/watch?v=TKt_VlMn_aA
+// (2) https://www.youtube.com/watch?v=B34iq4O5ZYI
+// (3) https://docs.unity3d.com/Manual/CollidersOverview.html
+// (4) https://noobtuts.com/unity/2d-pacman-game
+//-----------------------------------------------------------//
 using UnityEngine;
 
 [RequireComponent(typeof(Ghost))]
@@ -9,7 +20,7 @@ public class GhostBehavior : MonoBehaviour
     private void Awake()
     {
         this.ghost = GetComponent<Ghost>();
-        this.enabled = false;
+        //this.enabled = false;
     }
 
     public void Enable()
